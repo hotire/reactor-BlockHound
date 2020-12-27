96238f9321786b77631705e1b93cb2eabc5f6649
hotire <gngh0101@gmail.com>
2020-12-28 00:04:42 +0900

    Rename application

--- src/main/java/com/github/hotire/reactor/blockhound/Application.java
+++ src/main/java/com/github/hotire/reactor/blockhound/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactorBlockhoundApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReactorBlockhoundApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
