340d831d83b2b82ee81ee9c8402da3e5bfa71a33
hotire <gngh0101@gmail.com>
2020-12-28 00:04:11 +0900

    init

---
+++ src/main/java/com/github/hotire/reactor/blockhound/ReactorBlockhoundApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotire.reactor.blockhound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorBlockhoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorBlockhoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hotire/reactor/blockhound/ReactorBlockhoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotire.reactor.blockhound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorBlockhoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
