4120ee86face28d119b56f2be498ec4ac9551d5b
hotire <gngh0101@gmail.com>
2020-12-30 00:45:13 +0900

    Learn how to use ReactorDebugAgent

--- src/main/java/com/github/hotire/reactor/blockhound/Application.java
+++ src/main/java/com/github/hotire/reactor/blockhound/Application.java
@@ -3,13 +3,11 @@ package com.github.hotire.reactor.blockhound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import reactor.blockhound.BlockHound;
-
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        BlockHound.install();
+//        ReactorDebugAgent.init(); -> DebugAgentEnvironmentPostProcessor
         SpringApplication.run(Application.class, args);
     }
 
---
+++ src/main/java/com/github/hotire/reactor/blockhound/ReactorDebugAgentController.java
@@ -0,0 +1,18 @@
+package com.github.hotire.reactor.blockhound;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RequestMapping("/debug")
+@RestController
+public class ReactorDebugAgentController {
+
+    @GetMapping("/error")
+    public Mono<String> error() {
+        System.out.println(System.getProperty("reactor.tools.agent.installed"));
+        return Mono.just(1).map(i -> i + 1).flatMap(it -> Mono.error(new RuntimeException()));
+    }
+}
